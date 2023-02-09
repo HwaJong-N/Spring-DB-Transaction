92be7cc7f558ab5d67b5716d24f162280d211a64
HwaJong <hwajong1016@gmail.com>
2023-02-09 19:23:53 +0900

    트랜잭션 전파 활용

---
+++ src/main/java/ghkwhd/springtx/propagation/MemberService.java
@@ -0,0 +1,53 @@
+package ghkwhd.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    
+    // 트랜잭션을 각각 사용하는 예시
+    @Transactional
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+
+    @Transactional
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        // 로그에서 발생한 예외를 잡아서 정상 흐름으로 변환
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 반환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+}
